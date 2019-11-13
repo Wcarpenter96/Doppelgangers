a2f08cb5f2a165150e0c02d811cbbfa8174660d8
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-11-13 14:31:07 -0800

    configured aws s3

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const faceRoutes = require('./faceRoutes')
 const authRoutes = require('./authRoutes');
+const awsRoutes = require('./awsRoutes');
 
 // Every route inside of here
 // has /api
 router.use('/auth', authRoutes);
 router.use('/face', faceRoutes);
+router.use('/aws', awsRoutes);
 
 
 module.exports = router;
