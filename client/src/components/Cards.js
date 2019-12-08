4476be3d02b7c2024f4cb4b23f33aaf6ae882c33
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-12-07 20:35:52 -0800

    fixed bugs13

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -3,13 +3,17 @@ import { Card, Image, Placeholder } from 'semantic-ui-react'
 
 const CelebCard = matches => (
   <Card>
-    <Image src={matches[0].url ? <Placeholder><Placeholder.Image square /></Placeholder> : matches.celeb.url} wrapped ui={false} />
+    <Image src={matches.celeb.url ? matches.celeb.url
+      :
+      <Card.Content>
+        <Placeholder><Placeholder.Image square /></Placeholder>
+      </Card.Content>} wrapped ui={false} />
     <Card.Content>
-      <Card.Header>{matches[0].url ? matches.name : matches.celeb.name }</Card.Header>
+      <Card.Header>{matches.celeb.name}</Card.Header>
     </Card.Content>
     <Card.Content extra>
-        <p>{matches.confidence}%</p>
-      </Card.Content>
+      <p>{matches.confidence}%</p>
+    </Card.Content>
   </Card>
 )
 
