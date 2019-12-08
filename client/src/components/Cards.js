f43d0a9cdaf555c6892639587640531e2d3cb3ac
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-12-07 21:28:05 -0800

    fixed bugs15

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Card, Image } from 'semantic-ui-react'
 
-const CelebCard = matches => (
-  <Card>
+const CelebCard = matches => {
+  console.log('Matches=', matches);
+  console.log("matches.celeb.url=", matches.celeb.url);
+  console.log("matches.url=", matches.url)
+  return (
+    < Card >
     <Image src={matches.celeb.url} wrapped ui={false} />
     <Card.Content>
       <Card.Header>{matches.celeb.name}</Card.Header>
@@ -10,7 +14,9 @@ const CelebCard = matches => (
     <Card.Content extra>
       <p>{matches.confidence}%</p>
     </Card.Content>
-  </Card>
-)
+  </Card >
+  )
+}
+
 
 export default CelebCard;
